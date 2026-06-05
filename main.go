4c53396d8ea615d0cef847e91280cbdb997d9e86
Aliaksei Niaklesa <aliaksei.niaklesa@idt.net>
2026-06-05 13:32:57 +0200

    Added context

--- main.go
+++ main.go
@@ -27,25 +27,36 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
+	allMessages := []openai.ChatCompletionMessageParamUnion{
+		openai.SystemMessage("Add at start of every message 'HELLO THERE' text"),
+	}
+
 	for {
 		fmt.Printf("Write: ")
 
 		userInput, _ := reader.ReadString('\n')
 
+		allMessages = append(allMessages, openai.UserMessage(userInput))
+
 		response, err := client.Chat.Completions.New(
 			context.Background(),
 			openai.ChatCompletionNewParams{
-				Model: openai.ChatModelGPT4oMini,
-				Messages: []openai.ChatCompletionMessageParamUnion{
-					openai.UserMessage(userInput),
-				},
+				Model:    openai.ChatModelGPT4oMini,
+				Messages: allMessages,
+				// Temperature: openai.Float(2.0),
 			},
 		)
 
 		if err != nil {
 			fmt.Println("Error: ", err)
+			continue
 		}
 
-		fmt.Println(response)
+		fmt.Println("First -------", response.Choices[0].Message.Content)
+		fmt.Println("--------------------------------------------------------")
+
+		assistantResponse := response.Choices[0].Message.Content
+
+		allMessages = append(allMessages, openai.AssistantMessage(assistantResponse))
 	}
 }
